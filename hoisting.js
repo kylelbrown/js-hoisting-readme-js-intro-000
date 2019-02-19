e3e6e3111334ca85c4f3526b9a722ee229e91e81
Kyle Brown <kylelibrown@gmail.com>
2019-02-19 21:55:44 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -10,9 +10,10 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
+  var thisIsCrazy = function (){
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
+
     console.log("hey!!!")
   }
 }
